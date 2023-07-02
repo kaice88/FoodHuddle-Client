1c6aa500e29af69f40888d979186c6cd43d783a4
Huy Nguyen <anhhuy.nguyen@nfq.asia>
2023-07-02 11:18:48 +0700

    feat: :sparkles: add react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import { MantineProvider, Text } from '@mantine/core'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './Routes'
 
 export default function App() {
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS>
+      <RouterProvider router={router} />
       <Text>Welcome to Mantine!</Text>
     </MantineProvider>
   )
