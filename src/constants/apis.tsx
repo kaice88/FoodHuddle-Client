770d38b03481b10461d15fc49c693ff04dce44a8
lephuthuc <lephuthuc2001@gmail.com>
2023-07-27 13:31:21 +0700

    Changed Api Endpoint for GET all sessions Today

--- src/constants/apis.tsx
+++ src/constants/apis.tsx
@@ -3,5 +3,4 @@ export const REQUEST_GET_HOST_PAYMENT_INFO = "/v1/session/host-payment-infor";
 
 export const FOOD_ORDER = "/v1/food-order";
 
-export const REQUEST_GET_ALL_SESSIONS_TODAY =
-  "/v1/session/get-all-sessions-today";
+export const REQUEST_GET_ALL_SESSIONS_TODAY = "/v1/session/today";
