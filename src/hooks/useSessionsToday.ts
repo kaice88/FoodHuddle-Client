dcf22bd4773a3c97454d33e0781ad0eae2d26eac
nhung.phan <nhung.phan@nfq.com>
2023-07-29 22:10:23 +0700

    refactor: :art: fix conflict

--- src/hooks/useSessionsToday.ts
+++ src/hooks/useSessionsToday.ts
@@ -1,56 +1,56 @@
-import React, { useState } from "react";
+import { useState } from 'react'
 
-import { SessionsTodayPageTabs } from "@/enums";
-import axiosInstance from "@/settings/axios";
-import { useRequestProcessor } from "@/settings/react-query";
-import type { SessionToday } from "@/types/sessions";
-import { REQUEST_GET_ALL_SESSIONS_TODAY } from "@/constants/apis";
+import { SessionsTodayPageTabs } from '@/enums'
+import axiosInstance from '@/settings/axios'
+import { useRequestProcessor } from '@/settings/react-query'
+import type { SessionToday } from '@/types/sessions'
+import { REQUEST_GET_ALL_SESSIONS_TODAY } from '@/constants/apis'
 
-const { query } = useRequestProcessor();
+const { query } = useRequestProcessor()
 
 interface ApiResponse {
-  statusCode: number;
-  data: SessionToday[];
+  statusCode: number
+  data: SessionToday[]
 }
 
 const fetchSessionsToday = async (tab: SessionsTodayPageTabs) => {
   try {
-    const { data, status } = await axiosInstance.get<ApiResponse>(getUrl(tab));
-    if (status == 200) {
-      return data.data;
-    }
-  } catch (error) {
-    return [];
+    const { data, status } = await axiosInstance.get<ApiResponse>(getUrl(tab))
+    if (status == 200)
+      return data.data
   }
-};
+  catch (error) {
+    return []
+  }
+}
 
 const getUrl = (tab: SessionsTodayPageTabs): string => {
   switch (tab) {
-    case SessionsTodayPageTabs.ALL:
-      return REQUEST_GET_ALL_SESSIONS_TODAY;
+  case SessionsTodayPageTabs.ALL:
+    return REQUEST_GET_ALL_SESSIONS_TODAY
     default:
-      return REQUEST_GET_ALL_SESSIONS_TODAY;
+    return REQUEST_GET_ALL_SESSIONS_TODAY
   }
-};
+}
 
 const useSessionTodayData = (tab: SessionsTodayPageTabs) => {
   return query<SessionToday[], Error>(
-    ["sessionsToday", tab],
+    ['sessionsToday', tab],
     () => fetchSessionsToday(tab),
     {
       onSuccess: () => {
-        console.log("success");
+        console.log('success')
       },
-    }
-  );
-};
+    },
+  )
+}
 
 const useSessionsToday = (tab: SessionsTodayPageTabs) => {
-  const [activeTab, setActiveTab] = useState(tab);
+  const [activeTab, setActiveTab] = useState(tab)
 
-  const { data, isLoading, error } = useSessionTodayData(activeTab);
+  const { data, isLoading, error } = useSessionTodayData(activeTab)
 
-  return { activeTab, setActiveTab, data, isLoading, error };
-};
+  return { activeTab, setActiveTab, data, isLoading, error }
+}
 
-export default useSessionsToday;
+export default useSessionsToday
