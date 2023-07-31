9f6cd8f39c25efe8504d7c045ea7196bfd0f35af
lephuthuc <lephuthuc2001@gmail.com>
2023-07-31 11:30:51 +0700

    refactor: :sparkles: Refactored code for better readability

--- src/components/StatusBadge/index.tsx
+++ src/components/StatusBadge/index.tsx
@@ -1,14 +1,14 @@
-import React from "react";
-import { Badge } from "@mantine/core";
+import React from 'react'
+import { Badge } from '@mantine/core'
 
-import { StatusBadgeProps } from "./types";
+import type { StatusBadgeProps } from './types'
 
 function StatusBadge({ status }: StatusBadgeProps) {
   return (
     <Badge size="xl" variant="light">
       {status}
     </Badge>
-  );
+  )
 }
 
-export default StatusBadge;
+export default StatusBadge
--- src/constants/apis.ts
+++ src/constants/apis.ts
@@ -1,6 +1,8 @@
-export const REQUEST_AUTH_GOOGLE = "/v1/auth/google/callback";
+export const REQUEST_AUTH_GOOGLE = '/v1/auth/google/callback'
 
-export const REQUEST_POST_SESSION_INFO = "v1/session/create-new-session";
-export const REQUEST_GET_HOST_PAYMENT_INFO = "/v1/session/host-payment-infor";
+export const REQUEST_POST_SESSION_INFO = 'v1/session/create-new-session'
+export const REQUEST_GET_HOST_PAYMENT_INFO = '/v1/session/host-payment-infor'
 
-export const REQUEST_GET_ALL_SESSIONS_TODAY = "/v1/session/today";
+export const REQUEST_GET_ALL_SESSIONS_TODAY = '/v1/session/today'
+export const REQUEST_GET_HOSTED_SESSIONS_TODAY = '/v1/session/today/hosted'
+export const REQUEST_GET_JOINED_SESSIONS_TODAY = '/v1/session/today/joined'
